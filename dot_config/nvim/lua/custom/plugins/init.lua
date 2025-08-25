57f8b80e0aacedf732f36a56f27b144e4b69e930
sw4m <56038988+sw4m@users.noreply.github.com>
2025-08-25 19:21:19 +0200

    Update .config/nvim/lua/custom/plugins/init.lua

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -1,4 +1,9 @@
 return {
 	{ "ThePrimeagen/vim-be-good" },
-	{ "mrjones2014/smart-splits.nvim", build = "./kitty/install-kittens.bash" },
+	{
+		"mrjones2014/smart-splits.nvim",
+		build = "./kitty/install-kittens.bash",
+		lazy = true,
+		init = function() end,
+	},
 }
