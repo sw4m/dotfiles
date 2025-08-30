545300ebfaad1f01f9ab629631ae8cfd179a4d16
sw4m <56038988+sw4m@users.noreply.github.com>
2025-08-30 11:58:56 +0200

    Update .config/nvim/lua/custom/plugins/init.lua

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -39,4 +39,12 @@ return {
 			require("startup").setup()
 		end,
 	},
+	{
+		"AlexvZyl/nordic.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nordic").load()
+		end,
+	},
 }
