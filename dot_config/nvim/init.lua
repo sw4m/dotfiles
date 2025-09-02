5b21c66128ab78e247fe71b20bd1db2c05698fd3
sw4m <56038988+sw4m@users.noreply.github.com>
2025-09-02 09:57:02 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -57,7 +57,7 @@ vim.o.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-	vim.o.clipboard = "unnamedplus"
+	vim.o.clipboard = "unnamed"
 end)
 
 -- Enable break indent
