88905468c9722908c7d770624d85b5565b024293
sw4m <56038988+sw4m@users.noreply.github.com>
2025-09-18 15:23:40 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -385,7 +385,6 @@ require("lazy").setup({
 		dependencies = {
 			-- Automatically install LSPs and related tools to stdpath for Neovim
 			-- Mason must be loaded before its dependents so we need to set it up here.
-			-- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
 			{ "mason-org/mason.nvim", opts = {} },
 			"mason-org/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
@@ -865,9 +864,6 @@ require("lazy").setup({
 				"query",
 				"vim",
 				"vimdoc",
-				"php",
-				"html",
-				"javascript",
 			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
