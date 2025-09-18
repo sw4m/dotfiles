9934b64894eda29fa693ffb9c4121575fab5adc0
sw4m <56038988+sw4m@users.noreply.github.com>
2025-09-18 16:02:33 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -582,8 +582,9 @@ require("lazy").setup({
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-        pyright = {},
-        
+				pyright = {},
+				emmet_language_server = {},
+				css_ls = {},
 				-- clangd = {},
 				-- gopls = {},
 				-- rust_analyzer = {},
