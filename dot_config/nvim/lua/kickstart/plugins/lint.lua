af0cf7b16db32eb70c336d5f59d8f8b617a6d7f4
sw4m <56038988+sw4m@users.noreply.github.com>
2025-09-22 13:15:18 +0200

    Update .config/nvim/lua/kickstart/plugins/lint.lua

--- dot_config/nvim/lua/kickstart/plugins/lint.lua
+++ dot_config/nvim/lua/kickstart/plugins/lint.lua
@@ -7,7 +7,10 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
-        json = { 'jsonlint' },
+        json = { 'biome' },
+        html = {'htmlhint'},
+        javascript = {'biome'},
+        typescript = {'biome'},
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
