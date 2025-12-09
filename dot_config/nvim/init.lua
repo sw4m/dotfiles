845642f18c2edb00527dc0c3ffb020f7e1434b48
sw4m <56038988+sw4m@users.noreply.github.com>
2025-12-09 10:59:50 +0100

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -104,7 +104,7 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
-
+vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
 --  See `:help lua-guide-autocommands`
 
 -- Highlight when yanking (copying) text
@@ -665,7 +665,7 @@ require("lazy").setup({
 				python = { "isort", "black" },
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
-				javascript = { "prettierd", "prettier", stop_after_first = true },
+				javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
 			},
 		},
 	},
